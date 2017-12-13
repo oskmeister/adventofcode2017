99aa504e17226be497a278618b9994e3d27e05f3
Oskar Werkelin Ahlin <osk@spotify.com>
2017-12-13 20:43:15 +0100

    Part 2

--- 12/solve.cc
+++ 12/solve.cc
@@ -9,6 +9,15 @@ using namespace std;
 vector<int> neigh[MAX];
 bool vis[MAX];
 
+bool dfs(int at) {
+    if (vis[at]) return false;
+    vis[at] = true;
+    for (auto n : neigh[at]) {
+        dfs(n);
+    }
+    return true;
+}
+
 int main() {
     for (int i = 0; i < MAX; ++i) {
         int n;
@@ -35,4 +44,12 @@ int main() {
     int ans = 0;
     for (int i = 0; i < MAX; ++i) ans += vis[i];
     cout << ans << endl;
+
+    for (int i = 0; i < MAX; ++i) vis[i] = false;
+
+    int comp = 0;
+    for (int i = 0; i < MAX; ++i) {
+        comp += dfs(i) ? 1 : 0;
+    }
+    cout << comp << " components" << endl;
 }
