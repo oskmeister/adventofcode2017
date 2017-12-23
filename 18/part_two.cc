d2bf869391342f3f613d586e8d69412a05805161
Oskar Werkelin Ahlin <osk@spotify.com>
2017-12-23 23:35:28 +0100

    Blurgh

--- 18/part_two.cc
+++ 18/part_two.cc
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include <thread>
 #include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
@@ -14,9 +18,56 @@ bool is_number(const string &s) {
     return s[0] == '-' || (s[0] > '0' && s[0] <= '9');
 }
 
+queue<long long> rcv_queues[2];
+mutex mutexes[2];
+condition_variable cond[2];
+
+void execute(int id, const vector<Instr> &program) {
+    vector<long long> mem(26, 0);
+    mem['p' - 'a'] = id;
+    long long send_count = 0;
+    for (int i = 0; i < program.size(); ++i) {
+        const Instr &p = program[i];
+        long long val;
+        if (is_number(p.param)) {
+            val = stoi(p.param);
+        } else {
+            val = mem[p.param[0]-'a'];
+        }
+        if (p.op == "set") {
+            mem[p.mem-'a'] = val;
+        } else if (p.op == "mul") {
+            mem[p.mem-'a'] *= val;
+        } else if (p.op == "jgz") {
+            if (p.mem >= '0' && p.mem <= '9' && (p.mem-'0') > 0) {
+                i += val;
+                --i;
+            } else if (mem[p.mem-'a'] > 0) {
+                i += val;
+                --i;
+            }
+        } else if (p.op == "add") {
+            mem[p.mem-'a'] += val;
+        } else if (p.op == "mod") {
+            mem[p.mem-'a'] %= val;
+        } else if (p.op == "snd") {
+            std::unique_lock<std::mutex> lock(mutexes[id ^ 1]);
+            rcv_queues[id ^ 1].push(val);
+            cond[id ^ 1].notify_one();
+            ++send_count;
+        } else if (p.op == "rcv") {
+            std::unique_lock<std::mutex> lock(mutexes[id]);
+            cond[id].wait(lock, [&] { return !rcv_queues[id].empty(); });
+            long long v = rcv_queues[id].front();
+            rcv_queues[id].pop();
+            mem[p.param[0]-'a'] = v;
+        }
+        if (id == 1) cout << send_count << endl;
+    }
+}
+
 int main() {
     vector<Instr> program;
-    vector<long long> mem(26, 0);
     for (;;) {
         string input;
         char memory;
@@ -33,36 +84,8 @@ int main() {
         }
         program.push_back({input, memory, param});
     }
-    int lastplayed = -1;
-    for (int i = 0; i < program.size(); ++i) {
-        const Instr &p = program[i];
-        long long val;
-        if (is_number(p.param)) {
-            val = stoi(p.param);
-        } else {
-            val = mem[p.param[0]-'a'];
-        }
-        if (p.op == "set") {
-            mem[p.mem-'a'] = val;
-        } else if (p.op == "mul") {
-            mem[p.mem-'a'] *= val;
-        } else if (p.op == "jgz") {
-            if (mem[p.mem-'a'] > 0) {
-                i += val;
-                --i;
-            }
-        } else if (p.op == "add") {
-            mem[p.mem-'a'] += val;
-        } else if (p.op == "mod") {
-            mem[p.mem-'a'] %= val;
-        } else if (p.op == "snd") {
-            cout << "Playing " << val << endl;
-            lastplayed = val;
-        } else if (p.op == "rcv") {
-            if (mem[p.mem-'a'] != 0) {
-                cout << "Last played sound:" << lastplayed << endl;
-                return 0;
-            }
-        }
-    }
+    thread t1(execute, 0, program);
+    thread t2(execute, 1, program);
+    t1.join();
+    t2.join();
 }
