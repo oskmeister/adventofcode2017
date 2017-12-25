ae4eb5b22fec7e04f9f2957a7cf7bb317a8ed54b
Oskar Werkelin Ahlin <osk@spotify.com>
2017-12-25 15:45:10 +0100

    24 2

---
+++ 24/part_two.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<pair<int,int>> v;
+map<pair<int, long long>, pair<int,int>> cache;
+
+pair<int, int> solve(int cur, long long bits) {
+    if (cache.find(make_pair(cur, bits)) != cache.end())
+        return cache[make_pair(cur, bits)];
+    auto ans = make_pair(0,0);
+    for (int i = 0; i < v.size(); ++i) {
+        if ((1LL<<i) & bits) continue;
+        auto p = v[i];
+        const int to_add = p.first + p.second;
+        if (p.first == cur) {
+            auto p1 = solve(p.second, bits | (1LL << i));
+            p1 += 1;
+            p2 += to_add;
+            if (p1 > ans) ans = p1;
+        }
+        if (p.first == cur) {
+            auto p1 = solve(p.first, bits | (1LL << i));
+            p1 += 1;
+            p2 += to_add;
+            if (p1 > ans) ans = p1;
+        }
+    }
+    cache[make_pair(cur, bits)] = ans;
+    return ans;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        int a, b;
+        cin >> a >> b;
+        v.push_back(make_pair(a,b));
+    }
+
+    cout << solve(0, 0).second << endl;
+}
